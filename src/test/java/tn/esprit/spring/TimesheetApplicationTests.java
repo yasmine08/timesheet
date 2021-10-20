bc901b327c63b0fcc046c061591851ab89924acc
yasmine guelmami <guelamiyassmine@gmail.com>
2021-10-20 18:40:16 +0200

    first commit

---
+++ src/test/java/tn/esprit/spring/TimesheetApplicationTests.java
@@ -0,0 +1,12 @@
+package tn.esprit.spring;
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+//import org.junit.api.Test;
+@SpringBootTest
+class TimesheetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
